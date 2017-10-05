718d841f003eeb6de799bfbaf83e3cf635ce8e9d
Giacomo <giacomo.licari@gmail.com>
2017-10-05 16:03:36 +0200

    Docker automatically creates a sample market on docker-compose up

--- docker/gnosis_management_setup.sh
+++ docker/gnosis_management_setup.sh
@@ -4,5 +4,9 @@ echo "[GnosisDevKit] Setting Management up..."
 cd node_modules/@gnosis.pm/gnosis-core-contracts/
 echo "[GnosisDevKit] Migrating contracts..."
 truffle migrate --network development
+sleep 5
+cd ../../../ && node create_market.js
+sleep 5
 echo "[GnosisDevKit] Starting frontend..."
-cd ../../ && npm run start
+# cd ../../ && npm run start
+npm run start
--- docker/run_django.sh
+++ docker/run_django.sh
@@ -6,5 +6,6 @@ python manage.py migrate
 echo "==> Creating super user <== "
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@gnosis.pm').delete(); User.objects.create_superuser('admin', 'admin@gnosis.pm', 'admin')" | python manage.py shell
 echo "==> Super user credentials:\b\t==> user: admin | password: admin"
+python manage.py setup
 echo "==> Starting Django Server <=="
 python manage.py runserver 0.0.0.0:8000
