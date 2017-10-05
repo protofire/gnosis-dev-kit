0fcbac32975bdbd608f7fb85bf99b8db0860504a
Giacomo <giacomo.licari@gmail.com>
2017-10-05 17:37:43 +0200

    polished script

--- docker/gnosis_management_setup.sh
+++ docker/gnosis_management_setup.sh
@@ -6,7 +6,6 @@ echo "[GnosisDevKit] Migrating contracts..."
 truffle migrate --network development
 # Copy all generated files to /gnosis-core-contracts and share them via Volume
 cp -r * /gnosis-core-contracts && cd /gnosis-core-contracts && npm install
-# cd ../../../ &&
 sleep 5
 cd /gnosis-management
 node create_market.js
