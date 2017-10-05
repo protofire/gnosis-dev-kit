48d4f380598b11d07db95b4571a1fd17a60035ce
Giacomo <giacomo.licari@gmail.com>
2017-10-05 17:35:30 +0200

    gnosis-core-contracts shared from container to host, added sample dapp

--- docker/gnosis_management_setup.sh
+++ docker/gnosis_management_setup.sh
@@ -4,8 +4,12 @@ echo "[GnosisDevKit] Setting Management up..."
 cd node_modules/@gnosis.pm/gnosis-core-contracts/
 echo "[GnosisDevKit] Migrating contracts..."
 truffle migrate --network development
+# Copy all generated files to /gnosis-core-contracts and share them via Volume
+cp -r * /gnosis-core-contracts && cd /gnosis-core-contracts && npm install
+# cd ../../../ &&
 sleep 5
-cd ../../../ && node create_market.js
+cd /gnosis-management
+node create_market.js
 sleep 5
 echo "[GnosisDevKit] Starting frontend..."
 # cd ../../ && npm run start
